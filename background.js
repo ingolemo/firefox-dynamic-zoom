69e28c2cc76d22228bdf6db6b7264ad60640c1ef
Adrian Room <ingolemo@gmail.com>
2017-08-19 10:22:06 +0100

    switched to using futures

--- background.js
+++ background.js
@@ -1,48 +1,76 @@
-function set_width(num) {
-    return browser.storage.local.set({
-        width: num
-    }).then(rezoom_all)
-}
-
-function rezoom(tab) {
-    browser.storage.local.get('width').then(function(items) {
-        var width = items.width;
-        var zoom_level = 1;
-
-        if (!isNaN(width)) {
-            zoom_level = Math.max(0.3, Math.min(tab.width / width, 3));
-        }
-
-        browser.tabs.getZoom(tab.id).then(function(current_zoom) {
-            if (current_zoom != zoom_level) {
-                browser.tabs.setZoom(tab.id, zoom_level);
-            }
-        });
+function get_width() {
+    return browser.storage.local.get({
+        width: 1280
+    }).then(function(items) {
+        return items.width;
     });
 }
 
-function rezoom_all() {
+function set_width(num) {
+    return browser.storage.local.set({
+        width: num
+    }).then(function() {
+        rezoom_all(num)
+        return num;
+    });
+}
+
+function round(num) {
+    return Math.round(num * 100) / 100;
+}
+
+function rezoom(tab, width) {
+    var zoom_level = 1;
+
+    if (width != 0) {
+        zoom_level = tab.width / width
+        zoom_level = Math.max(0.3, Math.min(zoom_level, 3));
+    }
+
+    browser.tabs.getZoom(tab.id).then(function(current_zoom) {
+        if (round(current_zoom) != round(zoom_level)) {
+            browser.tabs.setZoom(tab.id, zoom_level);
+        }
+    });
+}
+
+function rezoom_all(width) {
     browser.tabs.query({
         "active": true,
     }, function(tabs) {
         for (tabData in tabs) {
-            rezoom(tabs[tabData]);
+            rezoom(tabs[tabData], width);
         }
     });
 }
 
+// set zoom the user switches tabs
 browser.tabs.onActivated.addListener(function(activeInfo) {
-    browser.tabs.get(activeInfo.tabId, rezoom);
+    get_width().then(function(width) {
+        browser.tabs.get(activeInfo.tabId, function(tab) {
+            rezoom(tab, width)
+        });
+    });
 });
 
+// set zoom the user navigates to a new url
 browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    rezoom(tab);
+    get_width().then(function(width) {
+        rezoom(tab, width);
+    });
 });
 
+// set zoom when the window is resized
 browser.runtime.onMessage.addListener(function(message, sender) {
     if (message.greeting === "resize") {
-        rezoom_all();
+        get_width().then(rezoom_all);
     }
 });
 
-setInterval(rezoom_all, 30 * 1000);
+// set zoom intermittantly (just in case I missed something)
+setInterval(function() {
+    get_width().then(rezoom_all);
+}, 30 * 1000);
+
+// set zoom when extension starts
+get_width().then(rezoom_all);
--- popup/choose_width.js
+++ popup/choose_width.js
@@ -1,34 +1,34 @@
-function set_selected_class() {
-    browser.storage.local.get("width").then(function(items) {
-        var value;
-        if (isNaN(items.width)) {
-            value = "Disable";
-        } else {
-            value = items.width.toString();
-        }
+var bgPage = browser.extension.getBackgroundPage();
 
-        choices = document.getElementsByClassName("choice");
-        var i;
-        for (i = 0; i < choices.length; i++) {
-            var choice = choices[i];
-            if (choice.textContent === value) {
-                choice.classList.add('selected');
-            } else {
-                choice.classList.remove('selected');
-            }
+function set_selected_class(width) {
+    var value = width.toString();
+    if (value === "0") {
+        value = "Disable";
+    }
+
+    choices = document.getElementsByClassName("choice");
+    var i;
+    for (i = 0; i < choices.length; i++) {
+        var choice = choices[i];
+        if (choice.textContent === value) {
+            choice.classList.add('selected');
+        } else {
+            choice.classList.remove('selected');
         }
-    });
+    }
 }
 
 document.addEventListener("click", function(e) {
     if (e.target.classList.contains("choice")) {
         var chosen_width = parseInt(e.target.textContent, 10);
-        chrome.extension.getBackgroundPage()
-            .set_width(chosen_width)
-            .then(set_selected_class);
+        if (isNaN(chosen_width)) {
+            chosen_width = 0;
+        }
+        console.log(toString(chosen_width))
+        bgPage.set_width(chosen_width).then(set_selected_class);
     }
 });
 
 document.addEventListener("DOMContentLoaded", function(e) {
-    set_selected_class();
+    bgPage.get_width().then(set_selected_class);
 });
